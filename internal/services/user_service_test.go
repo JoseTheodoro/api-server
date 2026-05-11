c8888df39410bbc9a422f22c50a99379e1bd8e7e
Jose Theodoro <jose.theodoro42@gmail.com>
2026-05-11 12:05:00 -0300

    adding test no specified error user service

--- internal/services/user_service_test.go
+++ internal/services/user_service_test.go
@@ -29,7 +29,7 @@ func (fr *fakeUserRepo) GetAll(ctx context.Context) ([]*domain.User, error) {
 	return nil, nil
 }
 
-func TestDeleteUser(t *testing.T) {
+func TestServiceDeleteUser(t *testing.T) {
 	var ctx context.Context = context.Background()
 
 	t.Run("success", func(t *testing.T) {
@@ -66,6 +66,20 @@ func TestDeleteUser(t *testing.T) {
 			t.Fatalf("expected domain.ErrUserNotFoud, got: %v", errGot)
 		}
 
+	})
+
+	t.Run("no specified error", func(t *testing.T) {
+		userFakeRepo := &fakeUserRepo{
+			DeleteFakeFn: func(ctx context.Context, id int) error {
+				return errors.New("no specified error")
+			},
+		}
+
+		userService := NewUserService(userFakeRepo)
+		errGot := userService.DeleteUser(ctx, 1)
+		if errGot == nil {
+			t.Fatalf("expected error instance, got: error nil")
+		}
 	})
 
 }
